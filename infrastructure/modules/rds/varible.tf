ea5f989822519dc9341ff9e43f916fe0dc7911e2
manoj kumar <manojkumarcm97@gmail.com>
2026-04-20 19:49:36 +0530

    golden ami

--- infrastructure/modules/rds/varible.tf
+++ infrastructure/modules/rds/varible.tf
@@ -28,13 +28,7 @@ variable "db_name" {
 variable "db_username" {
   description = "Database username"
   type        = string
-  sensitive   = true
   default = "master"
 }
 
-variable "db_password" {
-  description = "Database password"
-  type        = string
-  sensitive   = true
-  default = "56487"
-}
+variable "db_password" {}
